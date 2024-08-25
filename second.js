0a5293a175391704c8562dc2213206f3e434fe2f
OMsharma27 <158845302+OMsharma27@users.noreply.github.com>
2024-08-25 19:36:22 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,25 @@
+const images = [
+    "images/image1.jpg",
+    "images/image2.jpg",
+    "images/image3.jpg",
+    "images/image4.jpg",
+    "images/image5.jpg"
+];
+
+let currentIndex = 0;
+
+function showImage(index) {
+    const currentImage = document.getElementById('current-image');
+    currentIndex = index;
+    currentImage.src = images[currentIndex];
+}
+
+function prevImage() {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
+    showImage(currentIndex);
+}
+
+function nextImage() {
+    currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
+    showImage(currentIndex);
+}
